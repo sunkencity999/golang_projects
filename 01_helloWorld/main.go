6b2dbd1d1941e6ac31a527d258fde217966240de
Psydwannabe <soleil@corticalstack.net>
2015-12-31 12:03:51 -0800

    wrote a loop in Go

--- 01_helloWorld/main.go
+++ 01_helloWorld/main.go
@@ -3,5 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World!")
+	//fmt.Printf("%d - %b - %x \n", 42, 42, 42)
+	//prints 42 in decimal, than binary, thank base16/hexadecimal
+	for i := 0; i < 200; i++ {
+		fmt.Printf("%d \t %b \t %x \n", i, i, i)
+		//for the beginning ' i := 0' is where we initialize a 'counter' variable named
+		// 'i' that starts at/has the value of zero. Next we check a condition 'i, which has been initialized and given the
+		// value of 0, is less than the value of 200' post (the section after the ; )
+		// we instruct the program to do something after the loop code is run. in this
+		// instance we say to increment to the next number.
+	}
 }
